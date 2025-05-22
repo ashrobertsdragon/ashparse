7d053b1859389324754663ed5d86b9c2806516f2
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-05-21 22:30:26 -0600

    Add classes

--- src/ashparser/__init__.py
+++ src/ashparser/__init__.py
@@ -1,10 +1,18 @@
+"""The AshParser command line argument parser."""
+
+from .argument import Argument
+from .group import ArgumentGroup
+from .names import Names
 from .parser import Parser
-from .types_ import AshParser, ConditionalType, Argument, Names
+from .types_ import AshParser, ConditionalType, GroupType
 
 __all__ = [
     "Parser",
     "AshParser",
     "ConditionalType",
     "Argument",
+    "ArgumentGroup",
+    "GroupType",
+    "ConditionalType",
     "Names",
 ]
